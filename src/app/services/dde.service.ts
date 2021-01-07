8d4be415cb02b7ce528b2c55d86f46272d0afb7b
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-01-07 09:53:49 -0500

    update to latest dependencies

--- src/app/services/dde.service.ts
+++ src/app/services/dde.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { CookieService, CookieOptions } from 'ngx-cookie';
 import { Observable } from 'rxjs';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 import { COSService } from "./cos.service";
